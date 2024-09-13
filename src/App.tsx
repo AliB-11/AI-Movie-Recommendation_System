e0b22ebbed2bcc140e293ed70a3a4dd4f26d5526
Ali Bandali <alimbandali@gmail.com>
2024-09-12 21:37:22 -0400

    Add main header. Fix search filtering

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import MovieGrid from "./components/MovieGrid";
 import GenreList from "./components/GenreList";
 import { GenreObjects } from "./hooks/useGenres";
+import Heading from "./components/Heading";
 
 function App() {
   const [genre, setGenre] = useState<GenreObjects | null>(null);
@@ -27,10 +28,18 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingLeft={"20px"}>
-          <GenreList onSelectGenre={(genre) => setGenre(genre)} />
+          <GenreList
+            onSelectGenre={(genre) => {
+              setGenre(genre);
+              setSearch(null);
+            }}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
+        <HStack marginX={5} marginBottom={4}>
+          <Heading></Heading>
+        </HStack>
         <MovieGrid selectedGenre={genre} searchText={search}></MovieGrid>
       </GridItem>
     </Grid>
